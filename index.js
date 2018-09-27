879389f748f4b51d7cde682503cf70e465460b34
jojonarte <joselitonarte@gmail.com>
2018-09-27 15:10:23 +0800

    hooked all functions

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const { RNSimInfo } = NativeModules;
 
 export default {
   getCountryCode: () => RNSimInfo.countryCode,
+  getMobileCountryCode: () => RNSimInfo.mobileCountryCode,
+  getNetworkCode: () => RNSimInfo.mobileNetworkCode,
+  getCarrierName: () => RNSimInfo.carrierName,
 };
