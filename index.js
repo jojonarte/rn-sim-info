b7cec7973261ab22a565af9d071e51056f849345
Jojo Narte <jojonarte@users.noreply.github.com>
2019-10-17 17:03:39 +0800

    fix(typo): rename defaultValue to fallbackValue
     for more readability

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const isEmpty = value => value.trim().length === 0;
 const { countryCode, mobileCountryCode, mobileNetworkCode, carrierName } = RNSimInfo;
 
 export default {
-  getCountryCode: (defaultValue = '') => isEmpty(countryCode) ?  defaultValue : countryCode,
-  getMobileCountryCode: (defaultValue = '') => isEmpty(mobileCountryCode) ? defaultValue : mobileCountryCode,
-  getNetworkCode: (defaultValue = '') => isEmpty(mobileNetworkCode) ? defaultValue: mobileNetworkCode,
-  getCarrierName: (defaultValue = '') => isEmpty(carrierName) ? defaultValue: carrierName,
+  getCountryCode: (fallbackValue = '') => isEmpty(countryCode) ?  fallbackValue : countryCode,
+  getMobileCountryCode: (fallbackValue = '') => isEmpty(mobileCountryCode) ? fallbackValue : mobileCountryCode,
+  getNetworkCode: (fallbackValue = '') => isEmpty(mobileNetworkCode) ? fallbackValue: mobileNetworkCode,
+  getCarrierName: (fallbackValue = '') => isEmpty(carrierName) ? fallbackValue: carrierName,
 };
